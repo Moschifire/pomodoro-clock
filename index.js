4de0f53650390000b74d189591087354917c0a5c
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-01-21 21:00:53 +0100

    yet another commit

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 function App() {
-    return <div>Hello World</div>
+
+    const [displayTime, setDisplayTime] = React.useState(25 * 60);
+
+    const formatTime = (time) => {
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        return (minutes < 10 ? "0" + minutes : minutes) + 
+        ":" + 
+        (seconds < 10 ? "0" + seconds : seconds)
+    };
+    return (
+        <div>
+            <length title={"break length"} changeTime={null} type={"break"} time={null} formatTime={formatTime} />
+            <h1>{formatTime(displayTime)}</h1>
+        </div>
+    )
+}
+
+function length( title, changeTime, type, time, formatTime ) {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div className="time-sets">
+                <button className="btn-small deep-purple lighten-2">
+                    <i className="material-icons">arrow-downward</i>
+                </button>
+            </div>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
